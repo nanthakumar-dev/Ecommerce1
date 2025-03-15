827e6c4e2647bf8ec10a344eb5ed0bd722414f4f
nanthakumar-dev <nantha656565@gmail.com>
2025-03-15 11:13:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-   target:"https://ecommerce123-yixk.onrender.com"?,
+   target:"https://ecommerce123-yixk.onrender.com",
    changeOrigin:true,
    secure:false
 }
